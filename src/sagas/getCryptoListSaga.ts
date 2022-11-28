c0d09804ff861afbf7ddbc9e77248e6a9df5b0b0
FlorentLem <florentlempro@gmail.com>
2022-11-29 00:10:33 +0100

    wip added types

--- src/sagas/getCryptoListSaga.ts
+++ src/sagas/getCryptoListSaga.ts
@@ -1,13 +1,17 @@
 import { AxiosResponse } from "axios";
+import { CryptoResponse } from "../types/crypto";
 import { call } from "redux-saga/effects";
 import { request } from "../api/request";
 
 export function* getCryptoListSaga() {
   try {
-    const response: AxiosResponse<any> = yield call<typeof request>(request, {
-      method: "GET",
-      url: "https://open-api.coinglass.com/public/v2/funding",
-    });
+    const response: AxiosResponse<CryptoResponse> = yield call<typeof request>(
+      request,
+      {
+        method: "GET",
+        url: "https://open-api.coinglass.com/public/v2/funding",
+      }
+    );
 
     console.log(response.data);
   } catch (error) {
